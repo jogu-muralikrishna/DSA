be7f660e3409ca5860d3ed97a56ca6f6dae8218d
Jogu Muralikrishna <muralikrishnajogu@gmail.com>
2026-02-17 06:41:28 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,97 +1,32 @@
-// Node class
-class Node {
-    int data;
-    Node left, right;
+public class BinarySearch {
+    static int BinarySearch(int[]arr, int key){
+        int low = 0;
+        int high = arr.length-1;
 
-    Node(int key) {
-        data = key;
-        left = null;
-        right = null;
-    }
-}
+        while(low <=high) {
+            int mid = (low + high)/2;
 
-// Binary Search Tree class
-class BinarySearchTree {
-
-    Node root;
-
-    // -------- INSERT --------
-    Node insert(Node root, int key) {
-        if (root == null) {
-            return new Node(key);
+            if(arr[mid]== key) {
+                return mid;
+            }
+            else if(arr[mid] < key) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
         }
-
-        if (key < root.data) {
-            root.left = insert(root.left, key);
-        } else if (key > root.data) {
-            root.right = insert(root.right, key);
-        }
-
-        return root;
+        return -1;
     }
-
-    // -------- SEARCH --------
-    boolean search(Node root, int key) {
-        if (root == null)
-            return false;
-
-        if (root.data == key)
-            return true;
-
-        if (key < root.data)
-            return search(root.left, key);
-
-        return search(root.right, key);
-    }
-
-    // -------- INORDER -------- (Left Root Right)
-    void inorder(Node root) {
-        if (root != null) {
-            inorder(root.left);
-            System.out.print(root.data + " ");
-            inorder(root.right);
-        }
-    }
-
-    // -------- PREORDER -------- (Root Left Right)
-    void preorder(Node root) {
-        if (root != null) {
-            System.out.print(root.data + " ");
-            preorder(root.left);
-            preorder(root.right);
-        }
-    }
-
-    // -------- POSTORDER -------- (Left Right Root)
-    void postorder(Node root) {
-        if (root != null) {
-            postorder(root.left);
-            postorder(root.right);
-            System.out.print(root.data + " ");
-        }
-    }
-
-    // -------- MAIN --------
     public static void main(String[] args) {
+        int[] arr = {2,3,4,5,6,7,8,9};
+        int key = 5;
 
-        BinarySearchTree tree = new BinarySearchTree();
+        int result = BinarySearch(arr ,key);
 
-        // Insert nodes
-        int[] values = {25, 12, 50, 44, 17, 60};
-        for (int val : values) {
-            tree.root = tree.insert(tree.root, val);
-        }
-
-        System.out.println("Inorder Traversal:");
-        tree.inorder(tree.root);
-
-        System.out.println("\nPreorder Traversal:");
-        tree.preorder(tree.root);
-
-        System.out.println("\nPostorder Traversal:");
-        tree.postorder(tree.root);
-
-        int key = 17;
-        System.out.println("\n\nSearch " + key + ": " + tree.search(tree.root, key));
+        if(result != -1) 
+            System.out.println("Element is found at index:" + result);
+        else
+            System.out.println("element was not found");
     }
 }
